a4b89b7b18a105349194b7ce8b44f760b44c22c8
MxAziz <hereabdulaziz@gmail.com>
2024-12-06 22:20:05 +0600

    update newsletter section

--- src/components/outlets/homeroute/ExtraSection.jsx
+++ src/components/outlets/homeroute/ExtraSection.jsx
@@ -5,10 +5,10 @@ const ExtraSection = () => {
     <div>
       {/* extra section 01 */}
       <section className="bg-[#101622] ">
-        <h1 className=" text-4xl font-bold text-white w-2/3 mx-auto text-center py-14">
+        <h1 className=" text-3xl md:text-4xl font-bold text-white w-11/12 lg:w-2/3 mx-auto text-center py-14">
           Trusted by 25,000+ world-class brands and organizations of all sizes
         </h1>
-        <div className=" grid grid-cols-4 gap-6 pb-24 w-4/5 mx-auto text-white font-bold text-4xl">
+        <div className=" grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 pb-24 w-11/12 lg:w-4/5 mx-auto text-white font-bold text-xl md:text-3xl ">
           <h3>PAWPAW </h3>
           <h3>MoxChannel </h3>
           <h3>Mentheer </h3>
@@ -22,20 +22,22 @@ const ExtraSection = () => {
 
       {/* extra section 02 */}
       <section className="bg-[#19202f] py-16  text-white">
-        <div className="flex justify-between w-4/5 mx-auto">
+        <div className="md:flex justify-between w-11/12 lg:w-4/5 mx-auto">
           <h1 className="text-3xl font-bold">Newsletter Signup</h1>
-          <div className=" space-x-4">
+          <div className="mt-4 md:mt-0 grid grid-cols-1 md:grid-cols-2 md:gap-3 lg:gap-0 lg:grid-cols-3 space-y-3 md:space-y-0 md:space-x-2 lg:space-x-4">
             <input
               type="text"
               placeholder="Enter your Name"
               className="input input-bordered dark:bg-gray-700"
+              required
             />
             <input
               type="email"
               placeholder="Enter your Email"
               className="input input-bordered dark:bg-gray-700"
+              required
             />
-            <input className="btn" type="submit" value="SUBSCRIBE" />
+            <input className="btn md:mt-4 lg:mt-0" type="submit" value="SUBSCRIBE" />
           </div>
         </div>
       </section>
